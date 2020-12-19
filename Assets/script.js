05794bcb09976b9a2b6586786436aa99eb8eed3a
Skye Lucking <lucking.skye@gmail.com>
2020-12-18 20:22:31 -0700

    date format and 5 day forecast info is logging

--- Assets/script.js
+++ Assets/script.js
@@ -47,11 +47,6 @@ $(document).on("change", "#" + searchInput, function () {
 $(document).on("click", "#citySearch", function () {
   latData = document.getElementById("latitude_input").value;
   longData = document.getElementById("longitude_input").value;
-//   console.log(document.getElementById("latitude_input").value);
-//   console.log(
-//     "lat:" + latData + " long: " + longData + " City Name: " + cityName
-//   );
-
   var APIKey = "bc5f98e2e0cea0c6e28a5a426c201efa";
   var queryURL =
     "https://api.openweathermap.org/data/2.5/onecall?lat=" +
@@ -60,36 +55,25 @@ $(document).on("click", "#citySearch", function () {
     longData +
     "&exclude=&units=imperial&appid=" +
     APIKey;
-  $.ajax({
+  
+    $.ajax({
     url: queryURL,
     method: "GET",
   }).then(function (response) {
-    // Log the queryURL
-    // console.log(queryURL);
-
-    // Log the resulting object
-   
-    console.log(response);
-     console.log(response.daily[0]);
-    var date = new Date(response.daily[2].dt * 1000);
-   console.log(date);
-   let unix_timestamp = 1549312452
-   // Create a new JavaScript Date object based on the timestamp
-   // multiplied by 1000 so that the argument is in milliseconds, not seconds.
-   var date = new Date(unix_timestamp * 1000);
-   // Hours part from the timestamp
-   var hours = date.getHours();
-   // Minutes part from the timestamp
-   var minutes = "0" + date.getMinutes();
-   // Seconds part from the timestamp
-   var seconds = "0" + date.getSeconds();
-   
-   // Will display time in 10:30:23 format
-   var formattedTime = hours + ':' + minutes.substr(-2) + ':' + seconds.substr(-2);
-   
-   console.log(formattedTime);
-
-    // Render Weather Details
+        // Log the resulting object
+  console.log(response);
+  console.log(response.daily);
+  console.log(response.daily[0]);
+  var date = new Date(response.daily[0].dt * 1000);
+  console.log(date);
+        document.getElementById("cityNameLabel").innerHTML = cityName;
+        document.getElementById("currentDate").innerHTML = date;
+        document.getElementById("temp").innerHTML = response.current.temp;
+        document.getElementById("humidity").innerHTML = response.current.humidity;
+        document.getElementById("wind").innerHTML = response.current.wind_speed;
+        document.getElementById("uvi").innerHTML = response.current.uvi;
+    
+        // Render Weather Details
     for (var i = 0; i < 6; i++){
     function timeConverter(UNIX_timestamp){
         var UNIX_timestamp = response.daily[i].dt;
@@ -104,25 +88,25 @@ $(document).on("click", "#citySearch", function () {
         var time =  month + '/' + date+ '/' + year; 
         forecastDate = time;
         return time;
+        var forecast = 
+        document.getElementById("f0Date").innerHTML = cityName;
+        document.getElementById("f0Icon").innerHTML = date;
+        document.getElementById("temp").innerHTML = response.current.temp;
+        document.getElementById("f0Temp").innerHTML = response.current.humidity;
+        document.getElementById("fHumidity").innerHTML = response.current.wind_speed;
+        document.getElementById("uvi").innerHTML = response.current.uvi;
+        
       }
+      
       console.log(timeConverter(0));
+      console.log("Clouds: " + response.daily[i].clouds);
       
       var forecastDiv = $("<div>");
       forecastDiv.css("background-color", "red")
       var forecastDate = forecastDate;
       var pOne = $("<p>").text(forecastDate);
       forecastDiv.append(pOne);
-
-//     console.log(i)   
-//     var forecastDate = new Date(response.daily[i].dt * 1000);
-//    forecastDate = $(forecastDate, 'MMYY');
-//     console.log(forecastDate);
    }
-    document.getElementById("cityNameLabel").innerHTML = cityName;
-    document.getElementById("currentDate").innerHTML = date;
-    document.getElementById("temp").innerHTML = response.current.temp;
-    document.getElementById("humidity").innerHTML = response.current.humidity;
-    document.getElementById("wind").innerHTML = response.current.wind_speed;
-    document.getElementById("uvi").innerHTML = response.current.uvi;
+    
   });
 });
