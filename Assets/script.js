18f8ecd391289498f2c33ecddbf5afe267ee9cdd
Skye Lucking <lucking.skye@gmail.com>
2020-12-20 23:33:55 -0700

    everything is working! previous cities list and current weather.

--- Assets/script.js
+++ Assets/script.js
@@ -1,26 +1,36 @@
-// <!-- My Google Places API  Key = AIzaSyA5iZSuKAK0n-F7XktBgeLMAeY69bLd2Ls -->
-// <!-- Google Maps Javascript Library -->
-//Declare TimeBlocks Array
+// Rule of thumb for better organization.  Order 
+// 1) Global variable declarations
+// 2a) Function definitions
+// 2b) Good to define a function for initializing things/setup
+// 2c) Put code that initializes things or setup things inside the initialize function
+// 3) Last should be functional invocations or if using $(document).ready(function() { ... }), 
+// put the function that kicks off the app inside it
+
+
+//Declare Variables for City Search
 var savedCities = [];
-var searchInput = "search_input";
+
+var $searchInput = $("#search_input"); // document.querySelector('#search_iniput)
+var $citySearchBtn = $('#citySearch');
+var $latitudeInput = $('#latitude_input');
+var $prevCityList = $('#prevCityList');
 var longData = "";
 var latData = "";
 var cityName = "";
 var weatherResponse;
+var $prevCityBtn = $('.prevCityBtn');;
 
-$(document).ready(function () {
-  // preventDefault();
+
+function initGoogleMaps() {
   var autocomplete;
   autocomplete = new google.maps.places.Autocomplete(
-    document.getElementById(searchInput),
+    document.getElementById('search_input'),
     { types: ["geocode"] }
   );
   google.maps.event.addListener(autocomplete, "place_changed", function () {
     var near_place = autocomplete.getPlace();
-    document.getElementById(
-      "latitude_input"
-    ).value = near_place.geometry.location.lat();
-    document.getElementById(
+    $latitudeInput.value = near_place.geometry.location.lat();
+      document.getElementById(
       "longitude_input"
     ).value = near_place.geometry.location.lng();
 
@@ -30,30 +40,54 @@ $(document).ready(function () {
     document.getElementById(
       "longitutde-view"
     ).innerHTML = near_place.geometry.location.lng();
-    // console.log(near_place);
-    // console.log(document.getElementById("latitude_input").value);
-    // console.log(near_place.address_components[0].short_name);
-    cityName = near_place.address_components[0].short_name;
+       cityName = near_place.address_components[0].short_name;
   });
-});
+}
 
-$(document).on("change", "#" + searchInput, function () {
-  // var near_place = autocomplete.getPlace();
-  document.getElementById("latitude_input").value = "";
-  document.getElementById("longitude_input").value = "";
-  document.getElementById("latitude-view").innerHTML = "";
-  document.getElementById("longitutde-view").innerHtml = "";
-  longData = document.getElementById("longitude_input").value;
-  latData = document.getElementById("latitude_input").value;
-});
-
-$(document).on("click", "#citySearch", function () {
+function buildPastCitySearches() {
+  var city, $div;
   
-  latData = document.getElementById("latitude_input").value;
-  longData = document.getElementById("longitude_input").value;
-  getWeather(cityName, latData, longData);
+  $prevCityList.empty();
+  // console.log('we are in buildPastCitySearches');
+  for(var i = 0; i < savedCities.length; i++) {
+    console.log(savedCities.length);
+    city = savedCities[i];
+    $div = $('<button><div></div></button>')
+    .addClass('prevCityBtn alert-primary border border-dark ')
+    .attr("id", [i]);
+    $div.text(city.city);
+    $prevCityList.append($div);
+    $div.on('click', function(e) {
+     var id = $(this).attr('id');
+      console.log(id);
+      city = savedCities[id];
+      e.preventDefault();
+      getWeather(city.city, city.lat, city.long, false)
+    });
+  }
+}
 
-});
+function initialize() {
+  // Initialize everything
+  initGoogleMaps();
+  getFromLocalStorage();
+  buildPastCitySearches();
+
+  $searchInput.on('change', function() {
+    $latitudeInput.value = '';
+    document.getElementById("longitude_input").value = "";
+    document.getElementById("latitude-view").innerHTML = "";
+    document.getElementById("longitutde-view").innerHtml = "";
+    longData = document.getElementById("longitude_input").value;
+    latData = document.getElementById("latitude_input").value;
+  });
+
+  $citySearchBtn.on('click', function() {
+    latData = $latitudeInput.value;
+    longData = document.getElementById("longitude_input").value;
+    getWeather(cityName, latData, longData);
+  })
+}
 
 function getWeather(cityName, latData, longData){
   var forecastDate = "";
@@ -70,36 +104,15 @@ function getWeather(cityName, latData, longData){
     url: queryURL,
     method: "GET",
   }).then(function (response) {
-    console.log(response);
-    console.log(response.daily);
-    console.log(response.daily[0]);
     var date = new Date(response.daily[0].dt * 1000);
-    console.log(date);
     document.getElementById("cityNameLabel").innerHTML = cityName;
     document.getElementById("currentDate").innerHTML = date;
-    document.getElementById("temp").innerHTML = response.current.temp;
-    document.getElementById("humidity").innerHTML = response.current.humidity;
+    document.getElementById("temp").innerHTML = response.current.temp + " °F";
+    document.getElementById("humidity").innerHTML = response.current.humidity + " %";
     document.getElementById("wind").innerHTML = response.current.wind_speed;
     document.getElementById("uvi").innerHTML = response.current.uvi;
 
-    function saveCityArr() {
-      if ("localStorage" in window && window["localStorage"] !== null) {
-        var savedCity = {
-          city: cityName,
-          lat: response.lat,
-          long: response.lon,
-        };
-        // savedCities = localStorage.getItem('savedCity');
-        // savedCities = savedCities ? JSON.parse(savedCities) : [];
-        savedCities.push(savedCity);
-
-        localStorage.setItem("savedCity", JSON.stringify(savedCities));
-        //    alert("The data was saved.");
-        console.log(savedCities);
-      }
-    }
-
-    saveCityArr();
+    saveCityArr(response.lat, response.lon);  
     
     // Render 5 Day Forecast Weather Details
     $(".fBox").empty();
@@ -107,16 +120,7 @@ function getWeather(cityName, latData, longData){
       $(".fBox").css("visibility", "visible");
       //logging the forecast Information
       var date = new Date(response.daily[i].dt * 1000);
-      console.log(date);
-      console.log("Clouds: " + response.daily[i].clouds);
-      console.log(
-        "http://openweathermap.org/img/w/" +
-          response.daily[i].weather[0].icon +
-          ".png"
-      );
-      console.log("Temp: " + response.daily[i].temp.day + " F");
-      console.log("Humidity: " + response.daily[i].humidity);
-
+      
       //Converts Unix date to Formatted Date
       var UNIX_timestamp = response.daily[i].dt;
       var a = new Date(UNIX_timestamp * 1000);
@@ -144,10 +148,8 @@ function getWeather(cityName, latData, longData){
       var forecastDate = time;
 
       //Creating the Forecast Boxes
-      
       var newBox = $("<div>");
       var newCol = $("<col>");
-      //  $("newCol").attr('id', "forecast-" + i)
       var newDate = $("<div>");
       newDate.text(time);
       $("#fBox" + i)
@@ -172,12 +174,39 @@ function getWeather(cityName, latData, longData){
   });
 }
 
+function saveCityArr(lat,lon) {
+  var doesCityExist = false;
+  var savedCity;
+
+  if ("localStorage" in window && window["localStorage"] !== null) {
+    for(var i = 0; i < savedCities.length; i++) {
+      if (savedCities[i].city === cityName) {
+        doesCityExist = true;
+        break;
+      }
+    }
+    if (!doesCityExist) {
+      savedCity = {
+        city: cityName,
+        lat: lat,
+        long: lon,
+      };
+      savedCities.push(savedCity);
+      localStorage.setItem("savedCity", JSON.stringify(savedCities));
+    }
+    buildPastCitySearches();
+  }
+}
+
 function getFromLocalStorage() {
   // Check if local storage (LS) key exists
   if (localStorage.getItem("savedCity")) {
     // Then retrieve the associated value from LS
     savedCities = JSON.parse(localStorage.getItem("savedCity"));
-    console.log(savedCities);
   }
 }
-getFromLocalStorage();
+
+$(document).ready(function () {
+  initialize();
+});
+ 
