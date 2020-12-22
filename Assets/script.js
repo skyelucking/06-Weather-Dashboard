a8c193ad1462512d2626e4161b0b3005009f9ea7
Skye Lucking <lucking.skye@gmail.com>
2020-12-22 00:04:51 -0700

    didn't realize we needed to color code the UV index! Did that at the last minute...

--- Assets/script.js
+++ Assets/script.js
@@ -1,37 +1,36 @@
-// Rule of thumb for better organization.  Order 
+// Rule of thumb for better organization.  Order
 // 1) Global variable declarations
 // 2a) Function definitions
 // 2b) Good to define a function for initializing things/setup
 // 2c) Put code that initializes things or setup things inside the initialize function
-// 3) Last should be functional invocations or if using $(document).ready(function() { ... }), 
+// 3) Last should be functional invocations or if using $(document).ready(function() { ... }),
 // put the function that kicks off the app inside it
-
 
 //Declare Variables for City Search
 var savedCities = [];
 var $searchInput = $("#search_input"); // document.querySelector('#search_iniput)
-var $citySearchBtn = $('#citySearch');
-var $latitudeInput = $('#latitude_input');
-var $prevCityList = $('#prevCityList');
+var $citySearchBtn = $("#citySearch");
+var $latitudeInput = $("#latitude_input");
+var $prevCityList = $("#prevCityList");
 var longData = "";
 var latData = "";
 var cityName = "";
 var weatherResponse;
-var $prevCityBtn = $('.prevCityBtn');
+var $prevCityBtn = $(".prevCityBtn");
 var current_date = "";
 var current_time = "";
 
-
+//Autocomplete API
 function initGoogleMaps() {
   var autocomplete;
   autocomplete = new google.maps.places.Autocomplete(
-    document.getElementById('search_input'),
+    document.getElementById("search_input"),
     { types: ["geocode"] }
   );
   google.maps.event.addListener(autocomplete, "place_changed", function () {
     var near_place = autocomplete.getPlace();
     $latitudeInput.value = near_place.geometry.location.lat();
-      document.getElementById(
+    document.getElementById(
       "longitude_input"
     ).value = near_place.geometry.location.lng();
 
@@ -41,59 +40,57 @@ function initGoogleMaps() {
     document.getElementById(
       "longitutde-view"
     ).innerHTML = near_place.geometry.location.lng();
-       cityName = near_place.address_components[0].short_name;
+    cityName = near_place.address_components[0].short_name;
   });
 }
-
+//Build Previous Searched Cities List
 function buildPastCitySearches() {
   var city, $div;
-   $prevCityList.empty();
-  for(var i = 0; i < savedCities.length; i++) {
+  $prevCityList.empty();
+  for (var i = 0; i < savedCities.length; i++) {
     city = savedCities[i];
-    if (city.city === ""){
+    if (city.city === "") {
       // console.log("city exists");
-    } 
-    else {
-    $div = $('<button><div></div></button>')
-    .addClass('prevCityBtn alert-primary border border-dark ')
-    .attr("id", [i]);
-    $div.text(city.city);
-    $prevCityList.append($div);}
-    $div.on('click', function(e) {
-     var id = $(this).attr('id');
+    } else {
+      $div = $("<button><div></div></button>")
+        .addClass("prevCityBtn alert-primary border border-dark ")
+        .attr("id", [i]);
+      $div.text(city.city);
+      $prevCityList.append($div);
+    }
+    $div.on("click", function (e) {
+      var id = $(this).attr("id");
       // console.log(id);
       city = savedCities[id];
       e.preventDefault();
-      getWeather(city.city, city.lat, city.long, false)
+      getWeather(city.city, city.lat, city.long, false);
     });
   }
 }
-
 
 function initialize() {
   // Initialize everything
   initGoogleMaps();
   getFromLocalStorage();
   buildPastCitySearches();
- 
 
-  $searchInput.on('change', function() {
-    $latitudeInput.value = '';
+  $searchInput.on("change", function () {
+    $latitudeInput.value = "";
     document.getElementById("longitude_input").value = "";
     document.getElementById("latitude-view").innerHTML = "";
     document.getElementById("longitutde-view").innerHtml = "";
     longData = document.getElementById("longitude_input").value;
     latData = document.getElementById("latitude_input").value;
   });
-
-  $citySearchBtn.on('click', function() {
+//Search Button Click Event
+  $citySearchBtn.on("click", function () {
     latData = $latitudeInput.value;
     longData = document.getElementById("longitude_input").value;
     getWeather(cityName, latData, longData);
-  })
+  });
 }
-
-function getWeather(cityName, latData, longData){
+//Populate the Current Weather for Selected City
+function getWeather(cityName, latData, longData) {
   var forecastDate = "";
   var APIKey = "bc5f98e2e0cea0c6e28a5a426c201efa";
   var queryURL =
@@ -109,26 +106,44 @@ function getWeather(cityName, latData, longData){
     method: "GET",
   }).then(function (response) {
     var date = new Date(response.daily[0].dt * 1000);
-    document.getElementById("cityNameLabel").innerHTML = cityName  + "(" + current_date + ")"  + "<img id='theImg' src='http://openweathermap.org/img/w/" +
-    response.current.weather[0].icon +
-    ".png'/>";
+    document.getElementById("cityNameLabel").innerHTML =
+      cityName +
+      "(" +
+      current_date +
+      ")" +
+      "<img id='theImg' src='http://openweathermap.org/img/w/" +
+      response.current.weather[0].icon +
+      ".png'/>";
     document.getElementById("currentTime").innerHTML = current_time;
     document.getElementById("temp").innerHTML = response.current.temp + " °F";
-    document.getElementById("humidity").innerHTML = response.current.humidity + " %";
+    document.getElementById("humidity").innerHTML =
+      response.current.humidity + " %";
     document.getElementById("wind").innerHTML = response.current.wind_speed;
+    
     document.getElementById("uvi").innerHTML = response.current.uvi;
-    // console.log(response);
-    
+     if (response.current.uvi <= 2){
+      document.getElementById("uvi").style.backgroundColor = 'green';
+     } else if (response.current.uvi >= 2 && response.current.uvi <= 5){
+      document.getElementById("uvi").style.backgroundColor = 'yellow'
+     } else if (response.current.uvi >= 5 && response.current.uvi <= 7){
+      document.getElementById("uvi").style.backgroundColor = 'orange'
+     } else if (response.current.uvi >= 8 && response.current.uvi <= 10){
+      document.getElementById("uvi").style.backgroundColor = 'red'
+     } else if (response.current.uvi >= 11){
+      document.getElementById("uvi").style.backgroundColor = 'purple'
+     }
 
-    saveCityArr(response.lat, response.lon);  
-    
+    // console.log(response);
+
+    saveCityArr(response.lat, response.lon);
+
     // Render 5 Day Forecast Weather Details
     $(".fBox").empty();
     for (var i = 0; i < 6; i++) {
       $(".fBox").css("visibility", "visible");
       //logging the forecast Information
       var date = new Date(response.daily[i].dt * 1000);
-      
+
       //Converts Unix date to Formatted Date
       var today_timestamp = response.current.dt;
       var UNIX_timestamp = response.daily[i].dt;
@@ -160,25 +175,19 @@ function getWeather(cityName, latData, longData){
       var today_min = b.getMinutes();
       var sec = a.getSeconds();
       var today_sec = b.getSeconds();
-      var current_hour = today_hour + ":" + today_min+ ":" + today_sec;
+      var current_hour = today_hour + ":" + today_min + ":" + today_sec;
       var time = month + "/" + date + "/" + year;
       var today_time = today_month + "/" + today_date + "/" + today_year;
       current_date = today_time;
-      console.log(current_hour);
-      console.log(response);
       var forecastDate = time;
 
       //Converts from 24 to 12HR format
       function convert(current_hour) {
-        return moment(current_hour, 'HH:mm:ss').format('h:mm:ss A');
-         
-    }
-     current_time = convert(current_hour);
-      console.log("current hour:" + current_time);
-   
+        return moment(current_hour, "HH:mm:ss").format("h:mm:ss A");
+      }
+      current_time = convert(current_hour);
 
-      
-      //Creating the Forecast Boxes
+      //Creating the 5 Day Forecast Boxes
       var newBox = $("<div>");
       var newCol = $("<col>");
       var newDate = $("<div>");
@@ -205,12 +214,12 @@ function getWeather(cityName, latData, longData){
   });
 }
 
-function saveCityArr(lat,lon) {
+function saveCityArr(lat, lon) {
   var doesCityExist = false;
   var savedCity;
 
   if ("localStorage" in window && window["localStorage"] !== null) {
-    for(var i = 0; i < savedCities.length; i++) {
+    for (var i = 0; i < savedCities.length; i++) {
       if (savedCities[i].city === cityName) {
         doesCityExist = true;
         break;
@@ -230,7 +239,6 @@ function saveCityArr(lat,lon) {
 }
 
 function getFromLocalStorage() {
-  
   // Check if local storage (LS) key exists
   if (localStorage.getItem("savedCity")) {
     // Then retrieve the associated value from LS
--- README.md
+++ README.md
@@ -1,46 +1,31 @@
-# 06 Server-Side APIs: Weather Dashboard
+# Skye Lucking - 06 Weather Dashboard (with City Autocomplete)
 
-Developers are often tasked with retrieving data from another application's API and using it in the context of their own. Third-party APIs allow developers to access their data and functionality by making requests with specific parameters to a URL. Your challenge is to build a weather dashboard that will run in the browser and feature dynamically updated HTML and CSS.
+<h1>Description of application</h1>
+A weather dashboard that allows you to search for a city (included an autocomplete API to help you with that) and then get recent weather data. It also saves your past searches so you may review them later. 
 
-Use the [OpenWeather API](https://openweathermap.org/api) to retrieve weather data for cities. The documentation includes a section called "How to start" that will provide basic setup and usage instructions. Use `localStorage` to store any persistent data.
+<br>
+ Enjoy!
 
-## User Story
+<br>
 
-```
-AS A hangry vegan mother with kids that have allergies
-I WANT to see the nearby restaurants that I can filter by type and accomodations
-SO THAT I can get my grub on
-```
+<h1>Screenshot</h1>
+<img src="Assets/SS1.png"><br>
+<img src="Assets/SS2.png"><br>
+<img src="Assets/SS3.png"><br>
 
-## Acceptance Criteria
 
-```
-GIVEN a weather dashboard with form inputs
-WHEN I search for a city
-THEN I am presented with current and future conditions for that city and that city is added to the search history
-WHEN I view current weather conditions for that city
-THEN I am presented with the city name, the date, an icon representation of weather conditions, the temperature, the humidity, the wind speed, and the UV index
-WHEN I view the UV index
-THEN I am presented with a color that indicates whether the conditions are favorable, moderate, or severe
-WHEN I view future weather conditions for that city
-THEN I am presented with a 5-day forecast that displays the date, an icon representation of weather conditions, the temperature, and the humidity
-WHEN I click on a city in the search history
-THEN I am again presented with current and future conditions for that city
-WHEN I open the weather dashboard
-THEN I am presented with the last searched city forecast
-```
 
-The following image demonstrates the application functionality:
+<h1>Link to the deployed application</h1>
+<a href="https://skyelucking.github.io/06-Weather-Dashboard/">https://skyelucking.github.io/06-Weather-Dashboard/</a>
 
-![weather dashboard demo](./Assets/06-server-side-apis-homework-demo.png)
+<h1>Link to GitHub Repo</h1>
+<a href="https://github.com/skyelucking/06-Weather-Dashboard">https://github.com/skyelucking/06-Weather-Dashboard</a>
 
-## Review
+<hr>
 
-You are required to submit the following for review:
-
-* The URL of the deployed application.
-
-* The URL of the GitHub repository. Give the repository a unique name and include a README describing the project.
-
-- - -
-© 2019 Trilogy Education Services, a 2U, Inc. brand. All Rights Reserved.
+<h1>Now here is your joke...</h1> <br>
+What do you call a computer superhero?
+<br>
+<br>
+<br>
+<em>A screensaver</em>
